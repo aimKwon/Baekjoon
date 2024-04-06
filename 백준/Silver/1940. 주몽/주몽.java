5688e4fcd19ea6ea67d377c76833f78572dc9cd8
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-04-06 21:48:11 +0900

    Update 주몽.java

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.BitSet;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
