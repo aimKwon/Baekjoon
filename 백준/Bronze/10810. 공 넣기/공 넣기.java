62e350b95750d56c7f2cefe38a32b3b7f02905f6
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-04-14 18:30:10 +0900

    [Bronze III] Title: 공 넣기, Time: 148 ms, Memory: 15908 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] A = new int [N];
+
+        for (int l = 0; l < M; l++) {
+            st = new StringTokenizer(br.readLine());
+
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            for (int m = i-1; m < j; m++) {
+                A[m] = k;
+            }
+        }
+        for (int e : A) {
+            System.out.print(e + " ");
+        }
+
+    }
+}
