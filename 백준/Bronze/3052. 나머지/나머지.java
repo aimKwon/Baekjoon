42bdae77f84de586320859df3865cd84cb2e0f0e
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-04-14 19:30:19 +0900

    [Bronze II] Title: 나머지, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 
 public class Main {
 
@@ -8,29 +9,14 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] Array = new int [10];
+        HashSet<Integer> hashSet = new HashSet<>();
 
-        for (int i = 0; i < 10; i++) {
+        for(int i = 0; i <10; i++) {
             int A = (Integer.parseInt(br.readLine())) % 42;
-            Array[i] = A;
+            hashSet.add(A);
         }
 
-        for (int i = 0; i < 9; i++) {
-            for (int l = i+1; l< 10; l++) {
-                if (Array[i] == Array[l]) {
-                    Array[i] = -1;
-                }
-            }
-        }
+        System.out.println(hashSet.size());
 
-        int count = 10;
-
-        for (int i =0; i< 10; i++) {
-            if (Array[i]== -1) {
-                count--;
-            }
-        }
-
-        System.out.println(count);
     }
 }
