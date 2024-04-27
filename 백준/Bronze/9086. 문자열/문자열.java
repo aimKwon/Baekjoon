f448995e1ae52f970e86950c4f17af709d74a3e8
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-04-27 21:31:51 +0900

    [Bronze V] Title: 문자열, Time: 116 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,23 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
-public class Main {
-
+public class Main  {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < T; i++) {
-            String st = br.readLine();
-
-            String A = st.charAt(0) + "";
-            String B = st.charAt(st.length()-1) + "";
-
-            System.out.println(A+B);
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            System.out.println(str.substring(0, 1)+str.substring(str.length()-1, str.length()));
         }
     }
 }
