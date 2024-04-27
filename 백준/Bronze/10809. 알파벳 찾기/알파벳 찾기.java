9f5bbf3f863a6cd4d3557c020fad8af1656d3fe4
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-04-27 22:26:37 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,29 +1,28 @@
-import java.util.Scanner;
- 
-public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
- 
- 
-		int[] arr = new int[26];
-		
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = -1;
-		}
- 
-		String S = in.nextLine();
- 
-		for(int i = 0; i < S.length(); i++) {
-			char ch = S.charAt(i);
-    
-			if(arr[ch - 'a'] == -1) {	
-				arr[ch - 'a'] = i;
-			}
-		}
- 
-		for(int val : arr) {	
-			System.out.print(val + " ");
-		}
-	}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main  {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        int[] arr = new int[26];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = -1;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (arr[ch - 'a'] == -1) {
+                arr[ch - 'a'] = i;
+            }
+        }
+
+        for(int elements : arr) System.out.println(elements);
+    }
 }
