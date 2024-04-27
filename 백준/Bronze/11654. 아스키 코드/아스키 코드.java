5a0cbdc5abd9f05d0d96256798f569fe596d827d
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-04-27 19:38:39 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -3,16 +3,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String st = br.readLine();
+        String str = br.readLine();
 
-        int result = st.charAt(0);
+        int result = str.charAt(0);
 
         System.out.println(result);
-
     }
 }
