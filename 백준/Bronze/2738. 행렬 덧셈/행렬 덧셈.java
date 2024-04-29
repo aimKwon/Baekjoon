e8fe8b9ad5f87dc13cbd06b3776db458d22c0907
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-04-29 20:06:08 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 228 ms, Memory: 17456 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -13,31 +13,24 @@ public class Main  {
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
 
-        int[][] Aarr = new int[A][B];
-        int[][] Barr = new int[A][B];
+        int[][] arr = new int[A][B];
 
-        for (int i = 0; i < A; i++) {
-                st = new StringTokenizer(br.readLine());
-            for (int l = 0; l < B; l++) {
-                Aarr[i][l] = Integer.parseInt(st.nextToken());
-            }
-        }
         for (int i = 0; i < A; i++) {
             st = new StringTokenizer(br.readLine());
             for (int l = 0; l < B; l++) {
-                Barr[i][l] = Integer.parseInt(st.nextToken());
+                arr[i][l] = Integer.parseInt(st.nextToken());
             }
         }
+
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < A; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int l = 0; l < B; l++) {
-                Aarr[i][l] = Aarr[i][l] + Barr[i][l];
+                sb.append(arr[i][l] + Integer.parseInt(st.nextToken()) + " ");
             }
+            sb.append("\n");
         }
-        for (int i = 0; i < A; i++) {
-            for (int l = 0; l < B; l++) {
-                System.out.print(Aarr[i][l] + " ");
-            }
-            System.out.println();
-        }
+        System.out.println(sb);
     }
 }
