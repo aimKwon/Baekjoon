cd2e36938dab68c464c6cf0baaf50e8f6cceea06
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-05-01 21:58:06 +0900

    [Silver II] Title: 스택 수열, Time: 356 ms, Memory: 26808 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,43 +1,49 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Main {
+public class Main  {
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] A = new int[N];
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int [N];
 
-        for(int i =0; i <N; i++) {
-            A[i] = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
         Stack<Integer> stack = new Stack<>();
         int num = 1;
         boolean result = true;
-        StringBuffer bf = new StringBuffer();
-        for(int i=0; i<N; i++) {
-            int su = A[i];
-            if(su >= num) {
-                while(su>=num) {
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < N; i++) {
+            int A = arr[i];
+
+            if (A >= num) {
+                while (A >= num) {
                     stack.push(num++);
-                    bf.append("+\n");
+                    sb.append("+\n");
                 }
                 stack.pop();
-                bf.append("-\n");
+                sb.append("-\n");
             } else {
-                int n = stack.pop();
-                if(n > su) {
+                int B = stack.pop();
+                if (B > A) {
                     System.out.println("NO");
                     result = false;
                     break;
                 } else {
-                    bf.append("-\n");
+                    sb.append("-\n");
                 }
             }
         }
-        if(result==true) System.out.println(bf.toString());
+        if (result==true) {
+            System.out.println(sb.toString());
+        }
 
     }
 }
