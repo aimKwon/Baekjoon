392e2278fe97940a573541e9a8736609f963ad2c
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-05-04 22:03:32 +0900

    [Silver I] Title: 절댓값 힙, Time: 344 ms, Memory: 25552 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.PriorityQueue;
 
 public class Main  {
@@ -10,31 +13,35 @@ public class Main  {
 
         int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Integer> myQueue = new PriorityQueue<>((o1, o2) -> {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(new Comparator<Integer>() {
 
-            int first_abs = Math.abs(o1);
-            int second_abs = Math.abs(o2);
-
-            if (first_abs == second_abs) {
-                return o1 > o2 ? 1 : -1;
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if(Math.abs(o1) > Math.abs(o2)) {
+                    return Math.abs(o1) - Math.abs(o2);
+                }else if(Math.abs(o1) == Math.abs(o2)) {
+                    return o1 - o2;
+                }else {
+                    return -1;
+                }
             }
-
-            return first_abs - second_abs;
         });
 
-        for (int i = 0; i < N; i++) {
-            int request = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-            if (request == 0) {
-                if (myQueue.isEmpty()) {
-                    System.out.println("0");
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    sb.append("0").append("\n");
                 } else {
-                    System.out.println(myQueue.poll());
+                    sb.append(pq.poll()).append("\n");
                 }
             } else {
-                myQueue.add(request);
+                pq.offer(x);
             }
         }
-
+        System.out.println(sb);
     }
 }
