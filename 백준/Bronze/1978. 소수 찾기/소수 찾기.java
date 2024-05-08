c5a190d68825c114b86827fe898fc5944e4b9e20
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-05-08 21:00:42 +0900

    [Bronze II] Title: 소수 찾기, Time: 132 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main  {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int result = 0;
+
+        for (int i = 0; i < N; i++) {
+            int M = Integer.parseInt(st.nextToken());
+            if (M != 1) {
+                int count = 0;
+                for (int l = 2; l < M; l++) {
+                    if (M % l == 0) {
+                        count ++;
+                    }
+                }
+                if (count == 0) {
+                    result++;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
