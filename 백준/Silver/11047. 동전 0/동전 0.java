063aeaa47a98b7c602c181e5e781e24fbd058cbe
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-05-22 21:44:30 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,30 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
+        int[] array = new int[N];
+
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            array[i] = Integer.parseInt(br.readLine());
         }
 
-        int result = 0;
+        int count = 0;
         for (int i = N-1; i >= 0; i--) {
-            if (arr[i] > K) {
-                continue;
-            } else {
-                result += (K / arr[i]);
-                K = K % arr[i];
+            if (array[i] <= K) {
+                count += (K / array[i]);
+                K = K % array[i];
             }
         }
-        System.out.println(result);
+        System.out.println(count);
     }
 }
