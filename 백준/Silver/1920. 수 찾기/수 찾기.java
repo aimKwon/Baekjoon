0636092afd9c52627509fd77c95a400426c6a8af
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-05-22 19:10:38 +0900

    [Silver IV] Title: 수 찾기, Time: 1344 ms, Memory: 56896 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -10,30 +10,29 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int[] A = new int[N];
-
+        int[] Narr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            Narr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(A);
+        Arrays.sort(Narr);
 
         int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < M; i++) {
             boolean find = false;
             int target = Integer.parseInt(st.nextToken());
             int start = 0;
-            int end = A.length-1;
+            int end = Narr.length-1;
 
             while(start <= end) {
                 int mid_index = (start+end) / 2;
-                int mid_value = A[mid_index];
+                int mid_value = Narr[mid_index];
 
                 if (mid_value > target) {
                     end = mid_index - 1;
-                } else if( mid_value < target) {
+                } else if (mid_value < target) {
                     start = mid_index + 1;
                 } else {
                     find = true;
@@ -45,7 +44,6 @@ public class Main {
             } else {
                 System.out.println(0);
             }
-
         }
     }
 }
