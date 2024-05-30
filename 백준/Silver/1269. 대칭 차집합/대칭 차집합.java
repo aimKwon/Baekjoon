34a28e6a7a24d0dcedaa7584dcea95ba68cdfcb4
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-05-30 12:02:47 +0900

    [Silver IV] Title: 대칭 차집합, Time: 748 ms, Memory: 78212 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -2,39 +2,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        Set<Integer> setA = new HashSet<>();
-        Set<Integer> setB = new HashSet<>();
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        HashSet<Integer> hashSet = new HashSet<>();
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < a; i++) {
-            setA.add(Integer.parseInt(st.nextToken()));
-        }
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < b; i++) {
-            setB.add(Integer.parseInt(st.nextToken()));
+
+        for (int i = 0; i < A; i++) {
+            hashSet.add(Integer.parseInt(st.nextToken()));
         }
 
-        int ans = 0;
-        for(int num : setA) {
-            if(!setB.contains(num)) {
-                ans += 1;
-            }
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < B; i++) {
+            hashSet.add(Integer.parseInt(st.nextToken()));
         }
-        for(int num : setB) {
-            if(!setA.contains(num)) {
-                ans += 1;
-            }
-        }
-        System.out.println(ans);
+
+        int result = (A + B - hashSet.size()) * 2;
+        
+        System.out.println(A + B - result);
     }
 }
