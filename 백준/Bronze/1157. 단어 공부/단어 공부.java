b9abbb24a0aa06444037a33e3628c0405599af4f
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-06-12 22:24:49 +0900

    [Bronze I] Title: 단어 공부, Time: 256 ms, Memory: 21144 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -2,35 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main  {
+public class Main {
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String str = br.readLine().toUpperCase();
+
         int[] arr = new int[26];
-        String str = br.readLine();
 
         for (int i = 0; i < str.length(); i++) {
-
-            if ('A' <= str.charAt(i) && str.charAt(i) <= 'Z') {
-                arr[str.charAt(i) - 'A']++;
-
-            } else {
-                arr[str.charAt(i) -'a']++;
+            arr[str.charAt(i) - 'A']++;
+        }
+        
+        int max = 0;
+        for (int i = 1; i < 26; i++) {
+            if (arr[i] > arr[max]) {
+                max = i;
             }
         }
 
-        int max = -1;
-        char ch = '?';
-
+        int count = 0;
         for (int i = 0; i < 26; i++) {
-            if(arr[i] > max) {
-                max = arr[i];
-                ch = (char)(i+65);
-            } else if (arr[i] == max) {
-                ch = '?';
+            if (arr[max] == arr[i]) {
+                count++;
             }
         }
-        System.out.println(ch);
+
+        if (count >= 2) {
+            System.out.println("?");
+        } else {
+            System.out.println((char)(max + 'A'));
+        }
     }
 }
