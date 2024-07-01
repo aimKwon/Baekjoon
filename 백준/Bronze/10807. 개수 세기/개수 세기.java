c26a500a9e6cce68ab24f49052d100d0defd11a7
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-07-01 10:34:09 +0900

    Update 개수 세기.java

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
