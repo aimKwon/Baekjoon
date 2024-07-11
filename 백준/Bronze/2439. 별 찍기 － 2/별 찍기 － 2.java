e01614bbedfe2fbded3c2c3203936587ea3d7f2e
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-07-11 15:33:24 +0900

    Update 별 찍기 － 2.java

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
