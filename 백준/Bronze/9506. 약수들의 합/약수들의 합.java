76f1b79c0bb46b0e9cb9c039c884de5463b57397
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-07-14 18:38:26 +0900

    Update 약수들의 합.java

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main  {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
